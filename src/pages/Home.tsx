c5f50d82e86269e4a587b2a3d6c58c9c1ac23d28
norendren <dylan.jacoby@gmail.com>
2025-11-10 19:44:22 -0700

    homepage reorg

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -24,17 +24,6 @@ function Home() {
         <div className="container">
           <h2 className="section-title">Projects</h2>
 
-          <h3 className="subsection-title">Professional Work</h3>
-          <div className="projects-grid">
-            <div className="project-card">
-              <div className="project-header">
-                <h4>Coming Soon</h4>
-                <span className="project-tag">Professional</span>
-              </div>
-              <p>Professional projects will be showcased here.</p>
-            </div>
-          </div>
-
           <h3 className="subsection-title">TTRPG Tools</h3>
           <div className="projects-grid">
             <div className="project-card">
